122138ba8de896e22a82db15f0aca4d3384b0ab5
cryptocore1001 <crutugremido-9188@ac-cool.c4.fr>
2024-06-10 14:31:56 +0700

    update format of kyc config (#140)

--- kyc/social/contract.go
+++ kyc/social/contract.go
@@ -120,10 +120,10 @@ type (
 
 	kycConfigJSON struct {
 		Social1KYC struct {
-			XPostLink string `json:"x-post-link"` //nolint:tagliatelle // .
+			XPostLink string `json:"xPostLink"`
 		} `json:"social1-kyc"` //nolint:tagliatelle // .
 		Social2KYC struct {
-			XPostLink string `json:"x-post-link"` //nolint:tagliatelle // .
+			XPostLink string `json:"xPostLink"`
 		} `json:"social2-kyc"` //nolint:tagliatelle // .
 		DynamicDistributionSocialKYC []*struct {
 			XPostLink string        `json:"xPostLink"`
